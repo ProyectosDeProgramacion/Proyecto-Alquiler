eff3f672649c1d235156e7902b7f9d28dc5052c9
ignacio bravo <proyectosprogra3@gmail.com>
2024-05-21 08:24:05 +0200

    Creación de las clases propias de los módulos

---
+++ AlquilerCoches/Prediction.cpp
@@ -0,0 +1,12 @@
+#include "Concesionario.hpp"
+#include "Coche.hpp"
+
+class Prediction {
+public:
+	//cuanto estas dispuesto a pagar
+	//FUNCION ENCONTRAR COCHE UNICO PRIMERO MUESTRA MARCA Y MAS TARDE MODELO
+	//Mostrar tipoCombustible
+	//Cuanto anos atras se creó
+	//Kilometros del coche
+	//Mostrar como último el estado del coche
+};
