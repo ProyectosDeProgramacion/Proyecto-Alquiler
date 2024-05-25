1c5b3d8a828afae864d726a243e0ab8cb6ad866f
ignacio bravo <proyectosprogra3@gmail.com>
2024-05-25 14:18:52 +0200

    Edicion del módulo correspondiente al informe

--- AlquilerCoches/Informe.cpp
+++ AlquilerCoches/Informe.cpp
@@ -1,5 +1,23 @@
 #include "Coche.hpp"
 #include "Exceptions.hpp"
 #include "Compra.hpp"
+#include <fstream>
+#include "Concesionario.hpp"
+
+
+
+void InformeCompras(Concesionario concesionario, string filename) {
+
+	ofstream archivo(filename);
+	if (!archivo.is_open()) throw ExceptionEscrituraArchivo();
+	for (Compra* c : concesionario.getComprasRealizadas()) {
+		archivo << "\nInforme de Compras:\n\n--------------------------------\nA nombre de: " <<
+	}
+	
+
+	
+
+
+}
 
 
