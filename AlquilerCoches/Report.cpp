e01af475d63bb45c25323529811e8d9c93fa042c
Proyectos Programación <160734933+ProyectosDeProgramacion@users.noreply.github.com>
2024-05-26 12:58:36 +0200

    Update Report.cpp

--- AlquilerCoches/Report.cpp
+++ AlquilerCoches/Report.cpp
@@ -4,20 +4,10 @@
 #include <fstream>
 #include "Concesionario.hpp"
 
-
-
 void InformeCompras(Concesionario concesionario, string filename) {
-
 	ofstream archivo(filename);
 	if (!archivo.is_open()) throw ExceptionEscrituraArchivo();
 	for (Compra* c : concesionario.getComprasRealizadas()) {
-		archivo << "\nInforme de Compras:\n\n--------------------------------\nA nombre de: " << c->getDNICliente() << "\nLa compra se realizó el: " << c->getFechaInicio() << "\nCon un precio de: " << c->getPrecio() << "\nDatos del coche:\n" << concesionario.muestraCoche(concesionario.buscaCoche(c->getIDCoche()));
+		archivo << "\nInforme de Compras:\n\n--------------------------------\nA nombre de: " << c->getDNICliente() << "\nLa compra se realizÃ³ el: " << c->getFechaInicio() << "\nCon un precio de: " << c->getPrecio() << "\nDatos del coche:\n" << concesionario.muestraCoche(concesionario.buscaCoche(c->getIDCoche()));
 	}
-	
-
-	
-
-
 }
-
-
