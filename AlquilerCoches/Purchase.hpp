c7b43e7de0caaf96fc3adcf7b41449c5f88276cd
Proyectos Programación <160734933+ProyectosDeProgramacion@users.noreply.github.com>
2024-05-27 08:24:38 +0200

    Update Purchase.hpp
    Annotated Class.

--- AlquilerCoches/Purchase.hpp
+++ AlquilerCoches/Purchase.hpp
@@ -1,12 +1,13 @@
 // (.hpp) PURCHASE
 
+// The hpp is defined.
 #pragma once
 #ifndef PURCHASE_HPP
 #define PURCHASE_HPP
 
 #include <iostream>
 #include <string>
-#include "Car.hpp"
+#include "Car.hpp" // It is necessary for you to access the methods of the car class.
 using namespace std;
 
 class Purchase {
@@ -18,6 +19,7 @@ private:
     int CarID;
     string CustomerID;
 
+// Purchase data.
 public:
     Purchase(string Car, string Model, int Date, int CarID, string CustomerID);
     string getCar();
