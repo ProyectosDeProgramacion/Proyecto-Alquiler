27ac4021fab8851f1528f44582c0b8756fcdeca9
Proyectos Programación <160734933+ProyectosDeProgramacion@users.noreply.github.com>
2024-05-27 07:56:32 +0200

    Update Exceptions.hpp
    Annotated Class

--- AlquilerCoches/Exceptions.hpp
+++ AlquilerCoches/Exceptions.hpp
@@ -8,11 +8,13 @@
 #include <exception>
 using namespace std;
 
-class ExceptionError : public exception { //Excepcion general
+class ExceptionError : public exception { // General exception.
 public:
     ExceptionError(const char* message) : exception(message) {}
 };
 
+// They all derive from the ExceptionErrror.
+// It shows all the exceptions for each particular case that will later be shown in the classes related to them.
 class ExceptionReadingFile : public ExceptionError {
 public:
     ExceptionReadingFile() : ExceptionError("Error reading the file.") {}
