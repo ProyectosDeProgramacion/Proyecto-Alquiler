087b534ba15efefef73f5d47fdbddc96daa6d688
Proyectos Programación <160734933+ProyectosDeProgramacion@users.noreply.github.com>
2024-05-27 08:38:21 +0200

    Update Prediction.hpp
    Annotated Class

--- AlquilerCoches/Prediction.hpp
+++ AlquilerCoches/Prediction.hpp
@@ -1,15 +1,15 @@
-// (.hpp) PREDICTION
+// (.hpp) PREDICTION - Module 1: Made by Yago López.
 
 #pragma once
 #ifndef PREDICTION_HPP
 #define PREDICTION_HPP
-
+// The classes necessary for the operation of this branch are accessed.
 #include <iostream>
 #include "Dealership.hpp"
 #include "Car.hpp"
 using namespace std;
 
-class Prediction {
+class Prediction { // All the filters that the user will enter according to his preferences are displayed.
 public:
     vector<Car*> FilterByBrand(const vector<Car*> cars, const string& brand);
     vector<Car*> FilterByModel(const vector<Car*>& cars, const string& model);
