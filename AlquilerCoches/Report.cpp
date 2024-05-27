07ac07e4286af9165fd7442775ac413535efbfaf
Proyectos Programación <160734933+ProyectosDeProgramacion@users.noreply.github.com>
2024-05-27 08:51:15 +0200

    Adding extra comments in Report.cpp

--- AlquilerCoches/Report.cpp
+++ AlquilerCoches/Report.cpp
@@ -6,12 +6,12 @@
 #include "Dealership.hpp"
 #include "Report.hpp"
 #include <fstream>
-
+//saves enough data to recognize an old customer again. If you want to reconnect, you can log in since you have your data saved in a Customers.txt file.
 void Report::PurchaseReport(Dealership dealership, string filename) {
 	ofstream file(filename);
 	if (!file.is_open()) throw ExceptionWritingFile();
 	for (Purchase* c : dealership.getPurchases()) {
-		file << "\nInforme de Compras:\n\n--------------------------------\nA nombre de: " << c->getCustomerID() << "\nLa compra se realizó el: "; 
+		file << "\nInforme de Compras:\n\n--------------------------------\nA nombre de: " << c->getCustomerID() << "\nLa compra se realizÃ³ el: "; 
 		cout << c->getDate() << "\nCon un precio de: " << c->getPrice() << "\nDatos del coche:\n" << dealership.showCar(dealership.searchCar(c->getCarID()));
 	}
 }
